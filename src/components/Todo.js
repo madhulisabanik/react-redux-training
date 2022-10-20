125bb0b6e8a674ff71ff52465d2a4d3738d33c60
Ubuntu <azureuser@LearningAzureMadhulisa.nrn1vcdu4pmuzdgvynaqgo5ine.px.internal.cloudapp.net>
2022-10-20 07:14:17 +0000

    Todo List completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,22 +3,43 @@ import { useState } from 'react';
 export default function Todo(props){
 
     const [task, setTask] = useState('');
-    const [updated, setUpdated] = useState(task);
+    const [todoListState, setTodoListState] = useState(props.todoData);
 
+    // Storing the input text box value
     const handleChange = (event) => {
         setTask(event.target.value);
     };
 
+    // Adding new task in Todo list locally
     const handleClick = () => {
-        setUpdated(task);
-        console.log(task)
+        setTodoListState((prevTodoListState) => [
+            ...prevTodoListState, {
+                "id": prevTodoListState.length + 1,
+                "task": task,
+                "description": "This is a dummy task",
+                "isChecked": false
+            }
+        ])
+
+        // After adding the new task, clearing the input text box
+        setTask('');
     };
 
-    const todoList = props.todoData.map((item) => {
+    // Handle checked box event
+    const handleChecked = (event) => {
+        //console.log(event.target.id);
+        let list = [...todoListState];
+        const matchedIndex = todoListState.findIndex((el) => { return el.id === parseInt(event.target.id)})
+        list[matchedIndex].isChecked = true;
+        setTodoListState([...list]);
+    };
+
+    // Creating Todo list render function
+    const todoList = todoListState.map((item) => {
         return(
             <div key={item.id}>
                 <ul className="listStyle checkbox-group">
-                    <input type="checkbox" id={"checkbox"+item.id} />
+                    {item.isChecked ? <i className="fa-solid fa-check"></i> : <input type="checkbox" id={item.id} onClick={handleChecked} />}
                     <li>{item.task}</li> - <li>{item.description}</li>
                 </ul>
             </div>
@@ -27,7 +48,7 @@ export default function Todo(props){
     
     return (
         <div>
-            <input type="text" placeholder="Create new task..." className="create-task-box" onChange={handleChange} />
+            <input type="text" value={task} placeholder="Create new task..." className="create-task-box" onChange={handleChange} />
             <input type="submit" value="Submit" className="create-task-submit-btn" onClick={handleClick} />
             {todoList}
         </div>
